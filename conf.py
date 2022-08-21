68f128297b1dd121a9b4c5ab2106d798983ab9f6
Periurban-Initiative <88018855+Periurban-initiative@users.noreply.github.com>
2022-08-21 12:03:13 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -18,8 +18,6 @@ extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-extensions = ['sphinxemoji.sphinxemoji']
-sphinxemoji_style = 'twemoji'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
